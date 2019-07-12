2f1a4cef70fe63e3b6346e266c9e60215ea0494c
pjl34423 <38470128+pjl34423@users.noreply.github.com>
2019-07-12 16:36:37 -0400

    Accommodated for Firebase's File structure
    Moved the scripts directory to the public directory to satisfy Firebase. Tested and slightly modified register_script.

--- public/scripts/register_scripts.js
+++ public/scripts/register_scripts.js
@@ -2,16 +2,18 @@ function init() {
     //If there is already a user signed in, navigate to the home page
     var user = firebase.auth().currentUser
     if(user) {
-        window.open("../public/home.html", "_self")
+        window.open("../public/home.html", "_self");// for testing-PJ
     }
 }
 
 function registerUserInFirebase() {
 	var email = document.getElementById("register_email").value;
-	console.log(email);
+	//console.log(email);
     var p1 = document.getElementById("register_password").value;
+	//console.log(p1);
+	
     var p2 = document.getElementById("register_confirm_password").value;
-
+	//console.log(p2);
     if(p1 != p2) {
         alert("Passwords do not match!");
         return;
